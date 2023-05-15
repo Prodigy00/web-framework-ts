3bcdeb850b97c7c265ef5ee53d370830570e222c
Gideon Idowu <simplygiddy@gmail.com>
2023-05-15 11:10:22 +0100

    initial commit

---
+++ src/models/User.ts
@@ -0,0 +1,24 @@
+import { AxiosResponse } from 'axios';
+import { Attributes } from './Attributes';
+import { Eventing } from './Eventing';
+import { Model } from './Model';
+import { ApiSync } from './ApiSync';
+
+
+export interface UserProps {
+    id?: number;
+    name?: string;
+    age?: number;
+}
+
+const rootUrl = `http://localhost:3000/users`;
+
+export class User extends Model<UserProps> {
+   static buildUser(attrs: UserProps): User{
+    return new User(
+        new Attributes<UserProps>(attrs),
+        new Eventing(),
+        new ApiSync<UserProps>(rootUrl)
+    )
+   }
+}
